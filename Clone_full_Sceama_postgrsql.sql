c317e5cd6accbbfc86f69ecfc44070e442a55ed8
Kishor Sonawane <kishormsonawane@gmail.com>
2023-02-24 16:16:33 +0800

    Create Clone_full_Sceama_postgrsql.sql
    Final Code

---
+++ Clone_full_Sceama_postgrsql.sql
@@ -0,0 +1,26 @@
+--  copies sequences, tables, data, views & functions
+
+
+CREATE OR REPLACE FUNCTION clone_schema(source_schema text, dest_schema text) RETURNS void AS
+$BODY$
+DECLARE 
+  objeto text;
+  buffer text;
+BEGIN
+    EXECUTE 'CREATE SCHEMA ' || dest_schema ;
+
+    FOR objeto IN
+        SELECT table_name::text FROM information_schema.tables WHERE table_schema = source_schema
+    LOOP        
+        buffer := dest_schema || '.' || objeto;
+        EXECUTE 'CREATE TABLE ' || buffer || ' (LIKE ' || source_schema || '.' || objeto || ' INCLUDING CONSTRAINTS INCLUDING INDEXES INCLUDING DEFAULTS)';
+        EXECUTE 'INSERT INTO ' || buffer || '(SELECT * FROM ' || source_schema || '.' || objeto || ')';
+    END LOOP;
+
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+
+-- How to call ?
+SELECT clone_schema('old_schema','new_schema');
